27da05f34ca8cd4572eec56e22423f9e18295bb5
peterjbone <peterjbone41@gmail.com>
2025-02-21 20:34:44 -0500

    practicing the jsx syntax

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
 	const [count, setCount] = useState(0);
 	let subtitulo = "practicing react";
 	let auth = true;
+	let cuatro = 4;
+	let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"];
 
 	//*********************** APP COMPONENT
 	return (
@@ -22,7 +24,16 @@ function App() {
 			<h1>Vite + React</h1>
 			<hr />
 			<h2>{subtitulo}</h2>
+			<p>Cuatro más cuatro es igual a: {4 + cuatro}</p>
 			<p>{auth ? "usuario logeado" : "usuario NO logeado"}</p>
+			<p>
+				<b>Estciones del año</b>
+			</p>
+			<ul>
+				{estaciones.map((item) => (
+					<li>{item}</li>
+				))}
+			</ul>
 			<div>
 				<label htmlFor="nombre">Nombre:</label>
 				<input type="text" id="nombre" />
