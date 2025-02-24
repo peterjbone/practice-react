750ca92ff1d5d7ebc8725575b99b1cc00df9ebfd
peterjbone <peterjbone41@gmail.com>
2025-02-24 17:47:59 -0500

    practicing with conditional rendering

--- src/components/RenderizadoElementos.jsx
+++ src/components/RenderizadoElementos.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const RenderizadoElementos = () => {
-	return (
-		<div>
-			<h2>Renderizado de elementos</h2>
-		</div>
-	);
-};
+class RenderizadoElementos extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Renderizado de elementos</h2>
+			</div>
+		);
+	}
+}
 
 export default RenderizadoElementos;
