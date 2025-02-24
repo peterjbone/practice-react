97fcd47cd31e1c61dcf6b318a03fa4ac35556010
peterjbone <peterjbone41@gmail.com>
2025-02-24 17:47:12 -0500

    practicing with conditional rendering

---
+++ src/components/RenderizadoElementos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RenderizadoElementos = () => {
+	return (
+		<div>
+			<h2>Renderizado de elementos</h2>
+		</div>
+	);
+};
+
+export default RenderizadoElementos;
