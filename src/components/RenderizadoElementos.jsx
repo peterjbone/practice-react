298605718094228fa8db85a76082b497fdc919a8
peterjbone <peterjbone41@gmail.com>
2025-02-24 17:50:31 -0500

    practicing with conditional rendering

--- src/components/RenderizadoElementos.jsx
+++ src/components/RenderizadoElementos.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 class RenderizadoElementos extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			seasons: ["primavera", "verano", "otoño", "invierno"]
+		};
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Renderizado de elementos</h2>
+				<b>estaciones del año</b>
+				<ol></ol>
 			</div>
 		);
 	}
