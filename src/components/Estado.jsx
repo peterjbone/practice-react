e436f323fbc61e9aaa6f459d54be4b2529226a49
peterjbone <peterjbone41@gmail.com>
2025-02-23 19:26:39 -0500

    practicing with local states

--- src/components/Estado.jsx
+++ src/components/Estado.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-const Estado = () => {
+/* const Estado = () => {
 	return (
 		<div>
 			<h2>Estado</h2>
 		</div>
 	);
 };
+
+export default Estado; */
+
+class Estado extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Estado</h2>
+			</div>
+		);
+	}
+}
 
 export default Estado;
