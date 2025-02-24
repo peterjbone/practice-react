4353943d4cfc6b3d2643ba61c01e7be1905391fa
peterjbone <peterjbone41@gmail.com>
2025-02-23 19:38:09 -0500

    practicing with conditional rendering

--- src/components/RenderizadoCondicional.jsx
+++ src/components/RenderizadoCondicional.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const RenderizadoCondicional = () => {
+/* const RenderizadoCondicional = () => {
 	return <div>RenderizadoCondicional</div>;
 };
+
+export default RenderizadoCondicional;
+ */
+
+class RenderizadoCondicional extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Estado</h2>
+				<p>padre: {this.state.contador}</p>
+				<EstadoAHijo contadorHijo={this.state.contador} />
+			</div>
+		);
+	}
+}
 
 export default RenderizadoCondicional;
