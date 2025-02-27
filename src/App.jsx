728bf43ee186b85793469189869e368951e4ad7d
peterjbone <peterjbone41@gmail.com>
2025-02-26 22:09:22 -0500

    practicing with events

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Propiedades from "./components/Propiedades.jsx";
 import Estado from "./components/Estado.jsx";
 import RenderizadoCondicional from "./components/RenderizadoCondicional.jsx";
 import RenderizadoElementos from "./components/RenderizadoElementos.jsx";
-import Eventos from "./components/Eventos.jsx";
+import { EventosES6 } from "./components/Eventos.jsx";
 
 function App() {
 	let subtitulo = "practicing react";
@@ -65,7 +65,7 @@ function App() {
 				<hr />
 				<RenderizadoElementos />
 				<hr />
-				<Eventos />
+				<EventosES6 />
 			</div>
 		</>
 	);
--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class Eventos extends Component {
+export class EventosES6 extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -36,5 +36,3 @@ class Eventos extends Component {
 		);
 	}
 }
-
-export default Eventos;
