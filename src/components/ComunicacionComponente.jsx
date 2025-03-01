87546ed0eb6f24aab0ad77dbacae0304359a95ac
peterjbone <peterjbone41@gmail.com>
2025-02-28 19:19:10 -0500

    practicing with events

---
+++ src/components/ComunicacionComponente.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
+
+class Padre extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Comuniacion entre componentes</h2>
+			</div>
+		);
+	}
+}
