84151e687c45f832fcf72194ab8f4255fc8b06e8
peterjbone <peterjbone41@gmail.com>
2025-02-28 20:36:02 -0500

    life cycle

--- src/components/CicloVida.jsx
+++ src/components/CicloVida.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 export default class Padre extends Component {
+	constructor(props) {
+		super(props);
+		console.log(0, "el componente se inicializa, aun NO esta en el DOM");
+	}
+
 	render() {
 		return (
 			<div>
-				<h2>Ciclo de vida</h2>
+				<h2>Ciclo de vida de los componentes</h2>
 			</div>
 		);
 	}
