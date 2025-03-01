bfaf018209cc41488c6ed3a5576bb80f513b0676
peterjbone <peterjbone41@gmail.com>
2025-02-28 19:31:18 -0500

    practicing with components comunication

--- src/components/ComunicacionComponente.jsx
+++ src/components/ComunicacionComponente.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 
 export default class Padre extends Component {
+	state = {
+		contador: 0
+	};
+
+	incrementar = (e) => {
+		this.setState({
+			contador: this.state.contador + 1
+		});
+	};
+
 	render() {
 		return (
 			<div>
 				<h2>Comuniacion entre componentes</h2>
 				<Hijo mensaje="mensaje para hijo 1" />
-				<Hijo mensaje="mensaje para hijo 2" />
+				<Hijo mensaje="mensaje para hijo 2" incrementar={this.incrementar} />
 			</div>
 		);
 	}
 }
 
-function Hijo({ mensaje }) {
-	return <h3>{mensaje}</h3>;
+function Hijo({ mensaje, incrementar }) {
+	return (
+		<>
+			<h3>{mensaje}</h3>;<button onClick={incrementar}>Sumar</button>
+		</>
+	);
 }
