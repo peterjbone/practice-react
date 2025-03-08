3fe2f66a93dae6765af5750b87e0bf063df30195
peterjbone <peterjbone41@gmail.com>
2025-03-08 12:38:46 -0500

    practicing ajax in react

--- src/components/Ajax.jsx
+++ src/components/Ajax.jsx
@@ -45,9 +45,13 @@ export default class Ajax extends Component {
 		return (
 			<div>
 				<h2>peticiones asincronas en componentes de clase</h2>
-				{this.state.pokemons.map((item) => (
-					<Pokemon key={uuidv4()} name={item.name} avatar={item.avatar} />
-				))}
+				{this.state.pokemons.length === 0 ? (
+					<h3>Cargando...</h3>
+				) : (
+					this.state.pokemons.map((item) => (
+						<Pokemon key={uuidv4()} name={item.name} avatar={item.avatar} />
+					))
+				)}
 			</div>
 		);
 	}
