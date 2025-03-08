ef3932872061d5d8d0b9aa8fb5e2f30eadf6b7c6
peterjbone <peterjbone41@gmail.com>
2025-03-08 13:58:52 -0500

    practicing with hooks

--- src/components/ContadorHooks.jsx
+++ src/components/ContadorHooks.jsx
@@ -10,9 +10,17 @@ export default ContadorHooks;
 export default function ContadorHooks() {
 	const [contador, setContador] = useState(0);
 
+	const sumar = () => setContador(contador + 1);
+	const restar = () => setContador(contador - 1);
+
+	//************ COMPONENTE
 	return (
 		<div>
 			<h2>Hooks - useState</h2>
+			<nav>
+				<button onClick={sumar}>Sumar</button>
+				<button onClick={restar}>restar</button>
+			</nav>
 		</div>
 	);
 }
