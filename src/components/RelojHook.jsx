d47abab336e40b0084daaa027ad6e0e71c3f11df
peterjbone <peterjbone41@gmail.com>
2025-03-11 09:46:08 -0500

    practicing with useEffect

--- src/components/RelojHook.jsx
+++ src/components/RelojHook.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function RelojHook() {
+	const [hour, setHour] = useState(new Date().toLocaleTimeString());
+	const [visible, setVisible] = useState(false);
+
+	//* COMPONENTE
 	return (
 		<div>
 			<h2>Reloj Hook</h2>
