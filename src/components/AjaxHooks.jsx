aadd405018b512520f4bd7ce9c346436e8fc855c
peterjbone <peterjbone41@gmail.com>
2025-03-13 06:57:31 -0500

    practicing with ajax and hooks

--- src/components/AjaxHooks.jsx
+++ src/components/AjaxHooks.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+function Pokemon(props) {
+	return (
+		<figure>
+			<img src={props.avatar} alt={props.name} />
+			<figcaption>{props.name}</figcaption>
+		</figure>
+	);
+}
+
 const AjaxHooks = () => {
-	const [first, setfirst] = useState(second);
+	const [pokemons, setPokemons] = useState([]);
 
 	//************************** COMPONENTE
 	return (
