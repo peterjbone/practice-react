7fb4f320d88d467fb83298773c8e49eafe714685
peterjbone <peterjbone41@gmail.com>
2025-03-13 07:00:18 -0500

    practicing with ajax and hooks

--- src/components/AjaxHooks.jsx
+++ src/components/AjaxHooks.jsx
@@ -15,11 +15,11 @@ const AjaxHooks = () => {
 	//************************** COMPONENTE
 	return (
 		<div>
-			<h2>peticiones asincronas en componentes de clase</h2>
-			{this.state.pokemons.length === 0 ? (
+			<h2>peticiones asincronas con hooks</h2>
+			{pokemons.length === 0 ? (
 				<h3>Cargando...</h3>
 			) : (
-				this.state.pokemons.map((item) => (
+				pokemons.map((item) => (
 					<Pokemon key={uuidv4()} name={item.name} avatar={item.avatar} />
 				))
 			)}
