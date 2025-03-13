06cd0403f6c968b1d949c77830241729e517068d
peterjbone <peterjbone41@gmail.com>
2025-03-13 06:50:21 -0500

    practicing with ajax and hooks

--- src/components/AjaxHooks.jsx
+++ src/components/AjaxHooks.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const AjaxHooks = () => {
-	return <div>AjaxHooks</div>;
+	return (
+		<div>
+			<h2>peticiones asincronas en componentes de clase</h2>
+			{this.state.pokemons.length === 0 ? (
+				<h3>Cargando...</h3>
+			) : (
+				this.state.pokemons.map((item) => (
+					<Pokemon key={uuidv4()} name={item.name} avatar={item.avatar} />
+				))
+			)}
+		</div>
+	);
 };
 
 export default AjaxHooks;
