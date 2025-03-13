8e2b02893a8b8f1ddb7043ec013e35c135e039a3
peterjbone <peterjbone41@gmail.com>
2025-03-13 06:58:10 -0500

    practicing with ajax and hooks

--- src/components/AjaxHooks.jsx
+++ src/components/AjaxHooks.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Pokemon(props) {
+function Pokemon({ avatar, name }) {
 	return (
 		<figure>
-			<img src={props.avatar} alt={props.name} />
-			<figcaption>{props.name}</figcaption>
+			<img src={avatar} alt={name} />
+			<figcaption>{name}</figcaption>
 		</figure>
 	);
 }
