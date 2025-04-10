7d70060d3e78bd918631090c0b2f255c0117bd9d
peterjbone <peterjbone41@gmail.com>
2025-04-09 21:53:15 -0500

    custom hooks

--- src/components/CustomHooks.jsx
+++ src/components/CustomHooks.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CustomHooks = () => {
-	return <div>CustomHooks</div>;
+	return (
+		<>
+			<h2>Hooks personalizados</h2>
+		</>
+	);
 };
 
 export default CustomHooks;
