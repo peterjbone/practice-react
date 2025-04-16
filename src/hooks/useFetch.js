6adfbb63927d6aee4d6f812cf0d2474ddc323ab2
peterjbone <peterjbone41@gmail.com>
2025-04-15 22:49:35 -0500

    custom hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export const useFecth = (url) => {
-	const [first, setfirst] = useState(second);
-	const [first, setfirst] = useState(second);
-	const [first, setfirst] = useState(second);
+	const [first, setData] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, seteError] = useState(null);
 };
