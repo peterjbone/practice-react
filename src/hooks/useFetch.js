8d93109aeadbc878995c28360c08ac0a85a86348
peterjbone <peterjbone41@gmail.com>
2025-04-17 22:36:50 -0500

    custom hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,7 +5,28 @@ export const useFecth = (url) => {
 	const [isPending, setIsPending] = useState(true);
 	const [error, seteError] = useState(null);
 
-	useEffect(() => {}, [url]);
+	useEffect(() => {
+		const getPokemons = async (url) => {
+			let res = await fetch(url);
+			let json = await res.json();
+			//console.log(json);
+
+			json.results.forEach(async (item) => {
+				let res = await fetch(item.url);
+				let json = await res.json();
+
+				let pokemon = {
+					id: json.id,
+					name: json.name,
+					avatar: json.sprites.front_default
+				};
+
+				setPokemons(() => [...pokemons, pokemon]);
+			});
+		};
+
+		getPokemons();
+	}, [url]);
 
 	return { data, isPending, error };
 };
