658de822f05492744854d39a0562de1446387582
peterjbone <peterjbone41@gmail.com>
2025-04-25 21:12:11 -0500

    custom hooks in react

--- src/components/CustomHooks.jsx
+++ src/components/CustomHooks.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useFecth } from "../hooks/useFetch";
 
 const CustomHooks = () => {
-	console.log(useFecth);
+	let url = "https://pokeapi.co/api/v2/pokemon/";
+
+	//console.log(useFecth);
 
 	//* componente
 	return (
